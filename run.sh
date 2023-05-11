edb13bc931c8ca8ff879dc0b50ad8879c78fdd32
Abraham Lopez <abraham.lopez@botpress.com>
2023-05-11 19:08:45 -0400

    use AWS role for temporary credentials

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ docker run -it \
     -e ACCOUNT=$ACCOUNT \
     -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN \
     -p 8000:8000 \
     -v $(pwd)/account-data:/opt/cloudmapper/account-data \
     -v $(pwd)/web:/opt/cloudmapper/web \
