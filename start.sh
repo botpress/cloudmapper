83452b33cd262bd155721f8305dc470aa3152d86
Abraham Lopez <abraham.lopez@botpress.com>
2023-05-16 22:14:33 -0400

    use simplified CloudMapper permission set

--- start.sh
+++ start.sh
@@ -7,7 +7,8 @@ docker run -it \
     -e ACCOUNT=$ACCOUNT \
     -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-    -p 8000:8000 \
+    -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN \
+    -p 8888:8000 \
     -v $(pwd)/account-data:/opt/cloudmapper/account-data \
     -v $(pwd)/web:/opt/cloudmapper/web \
     cloudmapper
